e41bf77d8bf524ae49ece93cca13322ef3664958
itsjoesullivan <itsjoesullivan@gmail.com>
2013-12-21 15:15:06 -0500

    added instrument

---
+++ instruments/do.sh
@@ -0,0 +1,89 @@
+
+mkdir old_dirty/C2
+/Users/joe/opt/sox/sox old_dirty/raw.aif old_dirty/C2/raw.aif trim 0 80
+mkdir old_dirty/F+2
+/Users/joe/opt/sox/sox old_dirty/raw.aif old_dirty/F+2/raw.aif trim 80 80
+mkdir old_dirty/C3
+/Users/joe/opt/sox/sox old_dirty/raw.aif old_dirty/C3/raw.aif trim 160 80
+mkdir old_dirty/F+3
+/Users/joe/opt/sox/sox old_dirty/raw.aif old_dirty/F+3/raw.aif trim 240 80
+mkdir old_dirty/C4
+/Users/joe/opt/sox/sox old_dirty/raw.aif old_dirty/C4/raw.aif trim 320 80
+mkdir old_dirty/F+4
+/Users/joe/opt/sox/sox old_dirty/raw.aif old_dirty/F+4/raw.aif trim 400 80
+mkdir old_dirty/C5
+/Users/joe/opt/sox/sox old_dirty/raw.aif old_dirty/C5/raw.aif trim 480 80
+mkdir old_dirty/F+5
+/Users/joe/opt/sox/sox old_dirty/raw.aif old_dirty/F+5/raw.aif trim 560 80
+
+/Users/joe/opt/sox/sox old_dirty/C2/raw.aif old_dirty/C2/ppp.wav trim 0 10
+/Users/joe/opt/sox/sox old_dirty/C2/raw.aif old_dirty/C2/pp.wav trim 10 10
+/Users/joe/opt/sox/sox old_dirty/C2/raw.aif old_dirty/C2/p.wav trim 20 10
+/Users/joe/opt/sox/sox old_dirty/C2/raw.aif old_dirty/C2/mp.wav trim 30 10
+/Users/joe/opt/sox/sox old_dirty/C2/raw.aif old_dirty/C2/mf.wav trim 40 10
+/Users/joe/opt/sox/sox old_dirty/C2/raw.aif old_dirty/C2/f.wav trim 50 10
+/Users/joe/opt/sox/sox old_dirty/C2/raw.aif old_dirty/C2/ff.wav trim 60 10
+/Users/joe/opt/sox/sox old_dirty/C2/raw.aif old_dirty/C2/fff.wav trim 70 10
+
+/Users/joe/opt/sox/sox old_dirty/F+2/raw.aif old_dirty/F+2/ppp.wav trim 0 10
+/Users/joe/opt/sox/sox old_dirty/F+2/raw.aif old_dirty/F+2/pp.wav trim 10 10
+/Users/joe/opt/sox/sox old_dirty/F+2/raw.aif old_dirty/F+2/p.wav trim 20 10
+/Users/joe/opt/sox/sox old_dirty/F+2/raw.aif old_dirty/F+2/mp.wav trim 30 10
+/Users/joe/opt/sox/sox old_dirty/F+2/raw.aif old_dirty/F+2/mf.wav trim 40 10
+/Users/joe/opt/sox/sox old_dirty/F+2/raw.aif old_dirty/F+2/f.wav trim 50 10
+/Users/joe/opt/sox/sox old_dirty/F+2/raw.aif old_dirty/F+2/ff.wav trim 60 10
+/Users/joe/opt/sox/sox old_dirty/F+2/raw.aif old_dirty/F+2/fff.wav trim 70 10
+
+/Users/joe/opt/sox/sox old_dirty/C3/raw.aif old_dirty/C3/ppp.wav trim 0 10
+/Users/joe/opt/sox/sox old_dirty/C3/raw.aif old_dirty/C3/pp.wav trim 10 10
+/Users/joe/opt/sox/sox old_dirty/C3/raw.aif old_dirty/C3/p.wav trim 20 10
+/Users/joe/opt/sox/sox old_dirty/C3/raw.aif old_dirty/C3/mp.wav trim 30 10
+/Users/joe/opt/sox/sox old_dirty/C3/raw.aif old_dirty/C3/mf.wav trim 40 10
+/Users/joe/opt/sox/sox old_dirty/C3/raw.aif old_dirty/C3/f.wav trim 50 10
+/Users/joe/opt/sox/sox old_dirty/C3/raw.aif old_dirty/C3/ff.wav trim 60 10
+/Users/joe/opt/sox/sox old_dirty/C3/raw.aif old_dirty/C3/fff.wav trim 70 10
+
+/Users/joe/opt/sox/sox old_dirty/F+3/raw.aif old_dirty/F+3/ppp.wav trim 0 10
+/Users/joe/opt/sox/sox old_dirty/F+3/raw.aif old_dirty/F+3/pp.wav trim 10 10
+/Users/joe/opt/sox/sox old_dirty/F+3/raw.aif old_dirty/F+3/p.wav trim 20 10
+/Users/joe/opt/sox/sox old_dirty/F+3/raw.aif old_dirty/F+3/mp.wav trim 30 10
+/Users/joe/opt/sox/sox old_dirty/F+3/raw.aif old_dirty/F+3/mf.wav trim 40 10
+/Users/joe/opt/sox/sox old_dirty/F+3/raw.aif old_dirty/F+3/f.wav trim 50 10
+/Users/joe/opt/sox/sox old_dirty/F+3/raw.aif old_dirty/F+3/ff.wav trim 60 10
+/Users/joe/opt/sox/sox old_dirty/F+3/raw.aif old_dirty/F+3/fff.wav trim 70 10
+
+/Users/joe/opt/sox/sox old_dirty/C4/raw.aif old_dirty/C4/ppp.wav trim 0 10
+/Users/joe/opt/sox/sox old_dirty/C4/raw.aif old_dirty/C4/pp.wav trim 10 10
+/Users/joe/opt/sox/sox old_dirty/C4/raw.aif old_dirty/C4/p.wav trim 20 10
+/Users/joe/opt/sox/sox old_dirty/C4/raw.aif old_dirty/C4/mp.wav trim 30 10
+/Users/joe/opt/sox/sox old_dirty/C4/raw.aif old_dirty/C4/mf.wav trim 40 10
+/Users/joe/opt/sox/sox old_dirty/C4/raw.aif old_dirty/C4/f.wav trim 50 10
+/Users/joe/opt/sox/sox old_dirty/C4/raw.aif old_dirty/C4/ff.wav trim 60 10
+/Users/joe/opt/sox/sox old_dirty/C4/raw.aif old_dirty/C4/fff.wav trim 70 10
+
+/Users/joe/opt/sox/sox old_dirty/F+4/raw.aif old_dirty/F+4/ppp.wav trim 0 10
+/Users/joe/opt/sox/sox old_dirty/F+4/raw.aif old_dirty/F+4/pp.wav trim 10 10
+/Users/joe/opt/sox/sox old_dirty/F+4/raw.aif old_dirty/F+4/p.wav trim 20 10
+/Users/joe/opt/sox/sox old_dirty/F+4/raw.aif old_dirty/F+4/mp.wav trim 30 10
+/Users/joe/opt/sox/sox old_dirty/F+4/raw.aif old_dirty/F+4/mf.wav trim 40 10
+/Users/joe/opt/sox/sox old_dirty/F+4/raw.aif old_dirty/F+4/f.wav trim 50 10
+/Users/joe/opt/sox/sox old_dirty/F+4/raw.aif old_dirty/F+4/ff.wav trim 60 10
+/Users/joe/opt/sox/sox old_dirty/F+4/raw.aif old_dirty/F+4/fff.wav trim 70 10
+
+/Users/joe/opt/sox/sox old_dirty/C5/raw.aif old_dirty/C5/ppp.wav trim 0 10
+/Users/joe/opt/sox/sox old_dirty/C5/raw.aif old_dirty/C5/pp.wav trim 10 10
+/Users/joe/opt/sox/sox old_dirty/C5/raw.aif old_dirty/C5/p.wav trim 20 10
+/Users/joe/opt/sox/sox old_dirty/C5/raw.aif old_dirty/C5/mp.wav trim 30 10
+/Users/joe/opt/sox/sox old_dirty/C5/raw.aif old_dirty/C5/mf.wav trim 40 10
+/Users/joe/opt/sox/sox old_dirty/C5/raw.aif old_dirty/C5/f.wav trim 50 10
+/Users/joe/opt/sox/sox old_dirty/C5/raw.aif old_dirty/C5/ff.wav trim 60 10
+/Users/joe/opt/sox/sox old_dirty/C5/raw.aif old_dirty/C5/fff.wav trim 70 10
+
+/Users/joe/opt/sox/sox old_dirty/F+5/raw.aif old_dirty/F+5/ppp.wav trim 0 10
+/Users/joe/opt/sox/sox old_dirty/F+5/raw.aif old_dirty/F+5/pp.wav trim 10 10
+/Users/joe/opt/sox/sox old_dirty/F+5/raw.aif old_dirty/F+5/p.wav trim 20 10
+/Users/joe/opt/sox/sox old_dirty/F+5/raw.aif old_dirty/F+5/mp.wav trim 30 10
+/Users/joe/opt/sox/sox old_dirty/F+5/raw.aif old_dirty/F+5/mf.wav trim 40 10
+/Users/joe/opt/sox/sox old_dirty/F+5/raw.aif old_dirty/F+5/f.wav trim 50 10
+/Users/joe/opt/sox/sox old_dirty/F+5/raw.aif old_dirty/F+5/ff.wav trim 60 10
+/Users/joe/opt/sox/sox old_dirty/F+5/raw.aif old_dirty/F+5/fff.wav trim 70 10
